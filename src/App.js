63dd9e7296300704365705b494c2869e1a04000a
Philippine Lorrain <philippinelorrain@gmail.com>
2022-09-06 15:11:04 -0400

    having bugs with part of the course in my code when declaring the state like he does

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-// import logo from './logo.svg';
 import './App.css';
 import Member from './components/Member';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-      <Member name= 'Cyril'/>
-      <Member name= 'Johan'/>
-      <Member name= 'Marine'/>
-      <Member name= 'Anouk'>
-        <span>I am a Bouk</span>
-      </Member>
-      <Member name= 'Airi'/>
-    </div>
-  );
+const family = {
+  member1: {
+    name: 'Cyril',
+    age: 41
+  },
+  member2: {
+    name: 'Johan',
+    age: 38
+  },
+  member3: {
+    name: 'Marine',
+    age: 36
+  },
+  member4: {
+    name: 'Anouk',
+    age: 2
+  },
+  member5: {
+    name: 'Airi',
+    age: 28
+  },
 }
 
-export default App;
+function App() {
+  // line 7 creates a bug I don't know how to fix on
+  // the console/index.js/line 11 below {title}
+  // const { title } = this.props
+
+  // state = {
+  //   family
+  // }
+
+  return (
+      <div className="App">
+        <h1>React App</h1>
+        <Member name= 'Cyril' />
+        <Member name= 'Johan'/>
+        <Member name= 'Marine'/>
+        <Member name= 'Anouk'>
+          <span>I am a Bouk</span>
+        </Member>
+        <Member name= 'Airi'/>
+      </div>
+    );
+  }
+
+
+export default App
