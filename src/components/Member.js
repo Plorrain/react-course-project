9bedfdae8df0681c5ddbbec8df3a90caf48c828b
Philippine Lorrain <philippinelorrain@gmail.com>
2022-09-07 11:03:41 -0400

    adding css to the button and text + end of training project

--- src/components/Member.js
+++ src/components/Member.js
@@ -3,7 +3,9 @@ import React, { Fragment } from 'react'
 const Member = ({ name, age, children }) => {
   return (
     <Fragment>
-      <h2>
+      <h2 style={{
+        backgroundColor: age < 35 ? 'violet':'pink',
+        color: age < 35 ? '#7F8487':'white' }}>
         Family member: {name} : {age}
         { children ? <span>{children}</span> : <Fragment /> }
       </h2>
